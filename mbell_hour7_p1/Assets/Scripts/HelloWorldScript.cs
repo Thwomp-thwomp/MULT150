8b9630d22e5f790be42a91e2c14797f6eac4b481
Thwomp-thwomp <97mikeb@gmail.com>
2026-03-13 10:54:06 -0700

    hour 7 part 1

---
+++ mbell_hour7_p1/Assets/Scripts/HelloWorldScript.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+
+public class HelloWorldScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+
+
+    {
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
