2a9b2956dc410b4d74eac326add44fed456723ec
Thwomp-thwomp <97mikeb@gmail.com>
2026-05-03 14:12:19 -0700

    hour 11 part 2

--- mbell_hour11/Assets/Scripts/PrefabGenerator.cs
+++ mbell_hour11/Assets/Scripts/PrefabGenerator.cs
@@ -5,27 +5,18 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 spawnPosition = transform.position + new Vector3(i * 2f, 0, 0);
+            Instantiate(prefab, spawnPosition, transform.rotation);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B))
 
-        {
-            Instantiate(prefab);
-        }        
-                
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(prefab, transform.position, transform .rotation);
-        }
-                
-                
-                
     }
 }
