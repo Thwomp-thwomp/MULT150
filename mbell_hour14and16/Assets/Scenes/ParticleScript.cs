20f0df92c4447f6ca7715ac11a2225d19b363692
Thwomp-thwomp <97mikeb@gmail.com>
2026-05-10 15:46:47 -0700

    hour 16 scene

---
+++ mbell_hour14and16/Assets/Scenes/ParticleScript.cs
@@ -0,0 +1,7 @@
+//modifications (for fun I tried to make a train engine smoke effect)
+// 1. under emission burts changed cycles to infinite 
+// 2. changed color by speed to end with a light purple haze, changed start color to grey to make it look like dirty smoke
+// 3. changed the size from 1,1,1 to 3,3,3 to make the particles larger size in 3D start size 
+// 4. changed x value in force over lifetime to 3 to make smoke go to the right (ex: can simulate a cartoon train smoke). 
+// 5. changed the gravity modifer to a small negative value to make it look like the particles are falling upwards
+// 6. changed the simulation speed from 1 to 4 to look like the smoke from the train is moving faster 
