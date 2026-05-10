7c0f69eb1e781dceb181346595731340e716b9a3
Thwomp-thwomp <97mikeb@gmail.com>
2026-05-10 09:50:16 -0700

    hour 14 scene, TIY hour 14 and 16

---
+++ mbell_hour14and16/Assets/Scenes/UserInterface.cs
@@ -0,0 +1,6 @@
+//modifications for Michael's UI:
+// 1. changed imported background image to graffetti buildings 
+// 2. changed highlighted and pressed colors of the buttons 
+// 3. changed font to an 8 bit font that I imported 
+// 4. added two new buttons that when pressed it loads the "game" with either 1 or 2 players 
+// 5. added a logo/title that i made in another class for a game idea of mine called "Spray" :D
